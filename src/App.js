44e68375afdfc0e4c6ce6a30bb8b30e4f2cfaa87
Sambhav Gupta <sambhavgupta0705@gmail.com>
2023-02-21 19:56:25 +0530

    few changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-import './App.css';
+import "./App.css";
 import styled from "styled-components";
+import { useEffect ,useState} from "react";
+const BIRD_SIZE = 20;
+const GAME_WIDTH=500;
+const GAME_HEIGHT=500;
+
+
 function App() {
+  const [birdPosition,setBirdPosition]=useState(250);
+  
+
+  useEffect(() => {
+    
+  }, [])
   return (
-    <div className="App">
-      <h1>Flappy Bird</h1>
-    </div>
+    
+    <Div>
+      <GameBox height={GAME_HEIGHT} width={GAME_WIDTH}>
+      <Bird size={BIRD_SIZE} top={birdPosition}/>
+    </GameBox>
+    </Div>
   );
 }
 
 export default App;
+
+const Bird = styled.div`
+position: absolute;
+background-color:red;
+height:${(props) => props.size}px;
+width:${(props) => props.size}px;
+top:${(props) => props.size}px;
+border-radius: 50%;`;
+
+const Div=styled.div`
+display:flex;
+width:100%; 
+justify-content:center;`;
+
+const GameBox=styled.div`
+height:${(props) => props.height}px;
+width:${(props) => props.width}px;
+background-color:blue;`
+
